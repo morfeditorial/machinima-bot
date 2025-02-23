ef71d73cf48f7f3d1c313d657285a499c5be8980
Sergiy Chernega <chernegasergiy3@gmail.com>
2025-02-23 18:17:09 +0200

    feat: Add interfaces

---
+++ src/interfaces/DatabaseConfigInterface.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ *
+ *    _______   _______    _______   _______
+ *   /       \\/       \\//       \//       \
+ *  /        //        ///        //      __/
+ * /         /         /        _/        _/
+ * \__/__/__/\________/\____/___/\_______/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author CSSM Group
+ * @link https://cssm.pp.ua/
+ *
+ *
+ */
+
+declare(strict_types=1);
+
+namespace morfeditorial\interfaces;
+
+interface DatabaseConfigInterface
+{
+    public function getDsn() : string;
+
+    public function getUsername() : string;
+
+    public function getPassword() : string;
+
+    public function getOptions() : array;
+}
---
+++ src/interfaces/StorageInterface.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ *
+ *    _______   _______    _______   _______
+ *   /       \\/       \\//       \//       \
+ *  /        //        ///        //      __/
+ * /         /         /        _/        _/
+ * \__/__/__/\________/\____/___/\_______/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author CSSM Group
+ * @link https://cssm.pp.ua/
+ *
+ *
+ */
+
+declare(strict_types=1);
+
+namespace morfeditorial\interfaces;
+
+interface StorageInterface
+{
+    public function connect() : void;
+
+    public function query(string $query, array $params = []) : array;
+
+    public function execute(string $query, array $params = []) : void;
+}
