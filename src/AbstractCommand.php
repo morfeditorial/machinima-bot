c9a1db39247bd7cc6811bbbf2079bc7efb952e65
Sergiy Chernega <chernegasergiy3@gmail.com>
2025-02-23 18:32:07 +0200

    refactor: Update AbstractCommand.php logic

--- src/AbstractCommand.php
+++ src/AbstractCommand.php
@@ -23,6 +23,8 @@ declare(strict_types=1);
 
 namespace morfeditorial;
 
+use morfeditorial\interfaces\CommandInterface;
+
 abstract class AbstractCommand implements CommandInterface
 {
     protected MyBot $bot;
