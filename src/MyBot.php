292c8c59d7ce7e75a995edd5e6e97c2e69b77b24
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-06-27 16:11:47 +0300

    feat: Add /remove_role command to remove a role from a user

--- src/MyBot.php
+++ src/MyBot.php
@@ -157,6 +157,7 @@ class MyBot extends tgLib
         $this->command_factory->registerCommand(new \morfeditorial\commands\DeleteRoleCommand($this)); // delete_role
         $this->command_factory->registerCommand(new \morfeditorial\commands\AssignInitialAdminCommand($this)); // assign_initial_admin
         $this->command_factory->registerCommand(new \morfeditorial\commands\AssignRoleCommand($this)); // assign_role
+        $this->command_factory->registerCommand(new \morfeditorial\commands\RemoveRoleCommand($this)); // remove_role
 
         $this->command_factory->initializeCommands();
     }
---
+++ src/commands/RemoveRoleCommand.php
@@ -0,0 +1,79 @@
+<?php
+
+/*
+ *
+ *    _______   _______    _______   _______
+ *   /       \\/       \\//       \//       \
+ *  /        //        ///        //      __/
+ * /         /         /        _/        _/
+ * \__/__/__/\________/\____/___/\_______/
+ *
+ * This program is licensed under the CSSM Unlimited License v2.0.
+ * Copyright (c) 2024 Serhii Cherneha
+ *
+ * @author CSSM Group
+ * @link https://cssm.pp.ua/
+ *
+ *
+ */
+
+declare(strict_types=1);
+
+namespace morfeditorial\commands;
+
+use morfeditorial\AbstractCommand;
+use morfeditorial\MyBot;
+
+class RemoveRoleCommand extends AbstractCommand
+{
+    public function __construct(MyBot $bot)
+    {
+        parent::__construct($bot);
+        $this->setAliases(['remove_role']);
+        $this->setHiddenFromMenu(true);
+    }
+
+    public function getDescriptionKey() : string
+    {
+        return 'remove_role_command_description';
+    }
+
+    public function execute(
+        string $message,
+        int $message_id,
+        string $chat_type,
+        int $chat_id,
+        int $user_id,
+        $payload,
+        ?int $reply_message_id,
+        ?int $reply_author,
+        string $first_name,
+        $current_panel,
+        $current_page,
+        string $cmd,
+        array $args
+    ) : void {
+        if (! $this->isGranted('admin')) {
+            $this->bot->sendMessage($chat_id, $this->translate('no_permission_message'));
+
+            return;
+        }
+
+        if (count($args) < 2) {
+            $this->bot->sendMessage($chat_id, $this->translate('remove_role_usage_message'));
+
+            return;
+        }
+
+        $target_user_id = intval($args[0]);
+        $role_name = $args[1];
+
+        $result = $this->getRoleService()->removeUserRole($target_user_id, $role_name);
+
+        if ($result) {
+            $this->bot->sendMessage($chat_id, str_replace(['{roleName}', '{userId}'], [htmlspecialchars($role_name), $target_user_id], $this->translate('remove_role_message')));
+        } else {
+            $this->bot->sendMessage($chat_id, $this->translate('remove_role_failed_message'));
+        }
+    }
+}
