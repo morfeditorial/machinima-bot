77896423234d844c693086c214440212370de11d
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-06-28 23:15:36 +0300

    refactor: Migrate CategoryManageScreen to BaseMachinimaScreen

--- src/screens/Category/CategoryManageScreen.php
+++ src/screens/Category/CategoryManageScreen.php
@@ -21,42 +21,46 @@ declare(strict_types=1);
 
 namespace morfeditorial\screens\Category;
 
-use morfeditorial\screens\AbstractScreen;
+use morfeditorial\BaseMachinimaScreen;
 
-class CategoryManageScreen extends AbstractScreen
+class CategoryManageScreen extends BaseMachinimaScreen
 {
-    private ?int $parentId = null;
-
-    public function setParentId(?int $parentId) : self
+    public function supports(array $update): bool
     {
-        $this->parentId = $parentId;
-        return $this;
+        $action = $update['callback_query']['data'] ?? '';
+        return str_starts_with($action, 'category:manage');
     }
 
-    public function render() : void
+    public function handle(array $update): void
     {
+        $chatId = $update['callback_query']['message']['chat']['id'] ?? $update['message']['chat']['id'] ?? 0;
+        $userId = $update['callback_query']['from']['id'] ?? $update['message']['from']['id'] ?? 0;
+        $action = $update['callback_query']['data'] ?? '';
+
         if (! $this->isGranted('moderator')) {
-            $this->bot->sendMessage($this->chatId, $this->translate('no_permission_message'));
+            $this->client->sendMessage($chatId, $this->translate('no_permission_message'));
             return;
         }
 
-        $user_state_service = $this->bot->getContainer()->get('user_state_service');
-        $user_state_service->clearState($this->userId);
+        $this->getUserStateService()->clearState($userId);
 
-        $content_service = $this->bot->getContainer()->get('content_service');
+        $payload = $this->parsePayload($action);
+        $parentId = isset($payload['params'][0]) && $payload['params'][0] !== '' ? (int) $payload['params'][0] : null;
 
-        $categories = $content_service->getCategoriesByParent($this->parentId);
+        $content_service = $this->container->get('content_service');
+
+        $categories = $content_service->getCategoriesByParent($parentId);
         $keyboard = ['inline_keyboard' => []];
 
         foreach ($categories as $category) {
             $keyboard['inline_keyboard'][] = [
-                ['text' => $category['name'], 'callback_data' => $this->makePayload('category', 'manage', $category['id'])],
-                ['text' => '❌', 'callback_data' => $this->makePayload('category', 'delete', 'confirm', $category['id'])],
+                ['text' => $category['name'], 'callback_data' => $this->makePayload('category', 'manage', (string)$category['id'])],
+                ['text' => '❌', 'callback_data' => $this->makePayload('category', 'delete', 'confirm', (string)$category['id'])],
             ];
         }
 
-        if ($this->parentId) {
-            $add_callback = $this->makePayload('category', 'create', 'sub', $this->parentId);
+        if ($parentId) {
+            $add_callback = $this->makePayload('category', 'create', 'sub', (string)$parentId);
             $add_text = 'add_subcategory';
             $back_callback = $this->makePayload('category', 'manage');
         } else {
@@ -73,30 +77,28 @@ class CategoryManageScreen extends AbstractScreen
         ];
 
         $msg = $this->translate('manage_categories');
-        if ($this->parentId) {
-            $parent_category = $content_service->getCategoryById($this->parentId);
+        if ($parentId) {
+            $parent_category = $content_service->getCategoryById($parentId);
             if ($parent_category) {
                 $msg = str_replace('{category}', htmlspecialchars($parent_category['name']), $this->translate('manage_subcategories'));
             }
         }
 
-        $user_service = $this->bot->getContainer()->get('user_service');
-        $visuals_links = $this->bot->getContainer()->get('visuals_links');
-        $current_panel = $user_service->getCurrentPanel($this->userId);
+        $current_panel = $this->getUserService()->getCurrentPanel($userId);
 
-        $this->bot->editMediaMessage($this->chatId, $current_panel, $visuals_links[1], $msg, $keyboard);
-    }
-
-    public function handleCallback(string $action, array $params) : void
-    {
-        if ('manage' === $action) {
-            $this->parentId = isset($params[0]) ? (int) $params[0] : null;
-            $this->render();
+        if ($current_panel) {
+            $this->client->request('editMessageMedia', [
+                'chat_id' => $chatId,
+                'message_id' => $current_panel,
+                'media' => ['type' => 'photo', 'media' => $this->getVisualsLinks()[1], 'caption' => $msg, 'parse_mode' => 'HTML'],
+                'reply_markup' => $keyboard
+            ]);
+        } else {
+            $this->client->sendPhoto($chatId, $this->getVisualsLinks()[1], $msg, $keyboard);
         }
-    }
 
-    public function handleMessage(string $text) : void
-    {
-        // Not used
+        if (isset($update['callback_query']['id'])) {
+            $this->client->answerCallbackQuery($update['callback_query']['id']);
+        }
     }
 }
