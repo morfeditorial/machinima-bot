78d54cb93d2b4121dbf0745d8abdde4b67933fc3
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-06-29 10:53:56 +0300

    refactor: Add EntityManager to BaseMachinimaScreen

--- src/BaseMachinimaScreen.php
+++ src/BaseMachinimaScreen.php
@@ -21,29 +21,26 @@ declare(strict_types=1);
 
 namespace morfeditorial;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Contracts\Service\Attribute\Required;
 use Morfeditorial\TelegramBotBundle\Screen\AbstractScreen as BundleAbstractScreen;
-use morfeditorial\contracts\AuthorServiceInterface;
-use morfeditorial\contracts\RoleServiceInterface;
-use morfeditorial\contracts\UserServiceInterface;
-use morfeditorial\contracts\UserStateServiceInterface;
+use App\Service\RoleService;
+use App\Entity\User;
 
 abstract class BaseMachinimaScreen extends BundleAbstractScreen
 {
     protected ContainerInterface $container;
     protected Security $security;
+    protected EntityManagerInterface $em;
 
-    /**
-     * Using #[Required] allows Symfony to inject these automatically 
-     * without forcing us to redefine constructors in every child screen.
-     */
     #[Required]
-    public function setDependencies(ContainerInterface $container, Security $security): void
+    public function setDependencies(ContainerInterface $container, Security $security, EntityManagerInterface $em): void
     {
         $this->container = $container;
         $this->security = $security;
+        $this->em = $em;
     }
 
     public function getContainer(): ContainerInterface
@@ -61,24 +58,9 @@ abstract class BaseMachinimaScreen extends BundleAbstractScreen
         return $this->getTranslator()->translate($key, $params);
     }
 
-    public function getAuthorService(): AuthorServiceInterface
+    public function getRoleService(): RoleService
     {
-        return $this->container->get('author_service');
-    }
-
-    public function getUserService(): UserServiceInterface
-    {
-        return $this->container->get('user_service');
-    }
-
-    public function getUserStateService(): UserStateServiceInterface
-    {
-        return $this->container->get('user_state_service');
-    }
-
-    public function getRoleService(): RoleServiceInterface
-    {
-        return $this->container->get('role_service');
+        return $this->container->get(RoleService::class);
     }
 
     public function isGranted(string $role_name): bool
@@ -91,10 +73,6 @@ abstract class BaseMachinimaScreen extends BundleAbstractScreen
         return $this->container->get('visuals_links');
     }
 
-    /**
-     * Parse action and parameters from payload domain:action:arg1:arg2
-     * Returns ['domain' => 'domain', 'action' => 'action', 'params' => ['arg1', 'arg2']]
-     */
     protected function parsePayload(string $payload): array
     {
         $parts = explode(':', $payload);
@@ -108,21 +86,16 @@ abstract class BaseMachinimaScreen extends BundleAbstractScreen
         ];
     }
 
-    /**
-     * Create formatted payload for buttons
-     */
     protected function makePayload(string $domain, string $action, ...$params): string
     {
         $parts = [$domain, $action, ...$params];
         return implode(':', $parts);
     }
 
-    /**
-     * Render a panel: edit existing panel message or send a new photo.
-     */
     protected function renderPanel(int $chatId, int $userId, string $visual, string $caption, array $keyboard, bool $safe = false): void
     {
-        $currentPanel = $this->getUserService()->getCurrentPanel($userId);
+        $user = $this->em->find(User::class, $userId);
+        $currentPanel = $user?->getCurrentPanel();
 
         $editParams = [
             'chat_id' => $chatId,
