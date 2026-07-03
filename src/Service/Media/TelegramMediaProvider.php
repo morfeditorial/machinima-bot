93a8cbe7a4b2a051bcdc4baa5ac6af7d724bbb85
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-03 15:22:19 +0300

    feat: Implement TelegramMediaProvider in the bot bundle

---
+++ src/Service/Media/TelegramMediaProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Morfeditorial\Service\Media;
+
+use App\Service\Media\MediaProviderInterface;
+use Symfony\Component\DependencyInjection\Attribute\AsAlias;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+#[AsAlias(id: MediaProviderInterface::class)]
+class TelegramMediaProvider implements MediaProviderInterface
+{
+    public function __construct(
+        private HttpClientInterface $httpClient,
+        private string $botToken,
+    ) {}
+
+    public function getMediaUrl(string $fileId) : string
+    {
+        if (empty($this->botToken)) {
+            return 'default';
+        }
+
+        try {
+            $fileResponse = $this->httpClient->request('GET', "https://api.telegram.org/bot{$this->botToken}/getFile", [
+                'query' => ['file_id' => $fileId],
+            ]);
+
+            $fileData = $fileResponse->toArray(false);
+
+            if (!empty($fileData['result']['file_path'])) {
+                return "https://api.telegram.org/file/bot{$this->botToken}/".$fileData['result']['file_path'];
+            }
+        } catch (\Exception $e) {
+            // Ignore exceptions and fallback
+        }
+
+        return 'default';
+    }
+}
