c3eb933f262376e247c720c8a08746c67ef7f8f0
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-15 19:52:59 +0300

    refactor: Update Extension to load config from flat directory

--- src/DependencyInjection/MachinimaBotExtension.php
+++ src/DependencyInjection/MachinimaBotExtension.php
@@ -30,7 +30,7 @@ class MachinimaBotExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container) : void
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
         $loader->load('services.yaml');
     }
 }
