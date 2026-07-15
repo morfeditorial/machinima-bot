d3059f2c883cfda1d00c6fecbf7b335356920b61
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-15 10:32:14 +0300

    refactor(core): Migrate Author entity namespace to machinima-core

--- src/Screens/Author/AuthorEditNameScreen.php
+++ src/Screens/Author/AuthorEditNameScreen.php
@@ -21,7 +21,7 @@ declare(strict_types=1);
 
 namespace Morfeditorial\Screens\Author;
 
-use App\Entity\Author;
+use Morfeditorial\MachinimaCoreBundle\Entity\Author;
 use Morfeditorial\BaseMachinimaScreen;
 
 class AuthorEditNameScreen extends BaseMachinimaScreen
--- src/Screens/Staff/StaffAddScreen.php
+++ src/Screens/Staff/StaffAddScreen.php
@@ -21,7 +21,7 @@ declare(strict_types=1);
 
 namespace Morfeditorial\Screens\Staff;
 
-use App\Entity\Author;
+use Morfeditorial\MachinimaCoreBundle\Entity\Author;
 use Morfeditorial\BaseMachinimaScreen;
 
 class StaffAddScreen extends BaseMachinimaScreen
