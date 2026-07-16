30fdf252b4789b6c4abdf2862ef00ea458048641
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-16 12:46:25 +0300

    refactor: Use AuthorVoter in AuthorProfileScreen instead of isOwnProfile

--- src/Screens/Author/AuthorProfileScreen.php
+++ src/Screens/Author/AuthorProfileScreen.php
@@ -23,6 +23,7 @@ namespace Morfeditorial\MachinimaBotBundle\Screens\Author;
 
 use Morfeditorial\MachinimaBotBundle\BaseMachinimaScreen;
 use Morfeditorial\MachinimaCoreBundle\Entity\Author;
+use Morfeditorial\MachinimaCoreBundle\Security\Voter\AuthorVoter;
 
 class AuthorProfileScreen extends BaseMachinimaScreen
 {
@@ -55,22 +56,18 @@ class AuthorProfileScreen extends BaseMachinimaScreen
             $authorId = (int)($params[0] ?? 0);
             $author = $this->em->find(Author::class, $authorId);
 
-            $isOwnProfile = $author && $author->getUser() && (int) $author->getUser()->getId() === $userId;
-
-            if (!$this->isGranted('ROLE_MODERATOR') && !$isOwnProfile) {
+            if (!$author || !$this->isGranted(AuthorVoter::EDIT, $author)) {
                 $this->client->sendMessage($chatId, $this->translate('no_permission_message'));
                 return;
             }
 
-            if (null !== $author) {
-                $isPrivate = 'private' === $author->getState();
-                $privAuthor = $this->em->find(Author::class, $authorId);
-                if ($privAuthor) {
-                    $privAuthor->setState(!$isPrivate ? 'private' : 'public');
-                    $this->em->flush();
-                }
-                $this->renderProfile($chatId, $userId, $authorId);
+            $isPrivate = 'private' === $author->getState();
+            $privAuthor = $this->em->find(Author::class, $authorId);
+            if ($privAuthor) {
+                $privAuthor->setState(!$isPrivate ? 'private' : 'public');
+                $this->em->flush();
             }
+            $this->renderProfile($chatId, $userId, $authorId);
         } elseif ('unlink_telegram' === $route) {
             if (!$this->isGranted('ROLE_ADMIN')) {
                 return;
@@ -91,76 +88,61 @@ class AuthorProfileScreen extends BaseMachinimaScreen
 
         $author = $this->em->find(Author::class, $authorId);
 
-        $isOwnProfile = $author && $author->getUser() && (int) $author->getUser()->getId() === $userId;
-
-        if (!$this->isGranted('ROLE_MODERATOR') && !$isOwnProfile) {
+        if (!$author || !$this->isGranted(AuthorVoter::VIEW, $author)) {
             $this->client->sendMessage($chatId, $this->translate('no_permission_message'));
             return;
         }
 
         $visualsLinks = $this->getVisualsLinks();
 
-        if (null !== $author) {
-            $authorStatus = 'private' === $author->getState();
-            $currentPage = $this->userRepo->getCurrentPage($userId);
-
-            $keyboard = [
-                'inline_keyboard' => [
-                    [
-                        ['text' => $this->translate('change_name'), 'callback_data' => 'author:change_name:' . $authorId],
-                        ['text' => ($authorStatus ? $this->translate('make_public') : $this->translate('make_private')), 'callback_data' => 'author:set_private:' . $authorId],
-                    ],
-                    [
-                        ['text' => ($author->getBiography() ? $this->translate('change_bio') : $this->translate('add_bio')), 'callback_data' => 'author:edit_bio:' . $authorId],
-                        ['text' => ($author->getChannelLink() ? $this->translate('change_link') : $this->translate('add_link')), 'callback_data' => 'author:edit_link:' . $authorId],
-                    ],
-                ],
-            ];
-
-            if ($this->isGranted('ROLE_ADMIN')) {
-                if ($author->getUser()) {
-                    $keyboard['inline_keyboard'][] = [
-                        ['text' => $this->translate('unlink_telegram'), 'callback_data' => 'author:unlink_telegram:' . $authorId],
-                    ];
-                } else {
-                    $keyboard['inline_keyboard'][] = [
-                        ['text' => $this->translate('link_telegram'), 'callback_data' => 'author:link_telegram:' . $authorId],
-                    ];
-                }
-            }
-
-            if ($this->isGranted('ROLE_MODERATOR') || $isOwnProfile) {
-                $keyboard['inline_keyboard'][] = [
-                    ['text' => $this->translate('delete_this_author'), 'callback_data' => 'author:to_delete:' . $authorId],
-                ];
-            }
-
-            $keyboard['inline_keyboard'][] = [
-                ['text' => $this->translate('go_back'), 'callback_data' => $currentPage ? $currentPage : 'admin:panel'],
-            ];
-
-            $text = str_replace(
-                ['{author}', '{biography}', '{link}'],
-                [
-                    htmlspecialchars($author->getName()),
-                    ($author->getBiography() ? htmlspecialchars($author->getBiography()) : $this->translate('bio_not_set')),
-                    ($author->getChannelLink() ? htmlspecialchars($author->getChannelLink()) : $this->translate('link_not_set'))
-                ],
-                $this->translate('author_info_message')
-            );
-
-            $this->renderPanel($chatId, $userId, $visualsLinks[11], $text, $keyboard);
-            return;
-        }
+        $authorStatus = 'private' === $author->getState();
+        $currentPage = $this->userRepo->getCurrentPage($userId);
 
         $keyboard = [
             'inline_keyboard' => [
                 [
-                    ['text' => $this->translate('go_back'), 'callback_data' => 'admin:panel'],
+                    ['text' => $this->translate('change_name'), 'callback_data' => 'author:change_name:' . $authorId],
+                    ['text' => ($authorStatus ? $this->translate('make_public') : $this->translate('make_private')), 'callback_data' => 'author:set_private:' . $authorId],
+                ],
+                [
+                    ['text' => ($author->getBiography() ? $this->translate('change_bio') : $this->translate('add_bio')), 'callback_data' => 'author:edit_bio:' . $authorId],
+                    ['text' => ($author->getChannelLink() ? $this->translate('change_link') : $this->translate('add_link')), 'callback_data' => 'author:edit_link:' . $authorId],
                 ],
             ],
         ];
 
-        $this->renderPanel($chatId, $userId, $visualsLinks[1], $this->translate('author_not_found_message'), $keyboard);
+        if ($this->isGranted('ROLE_ADMIN')) {
+            if ($author->getUser()) {
+                $keyboard['inline_keyboard'][] = [
+                    ['text' => $this->translate('unlink_telegram'), 'callback_data' => 'author:unlink_telegram:' . $authorId],
+                ];
+            } else {
+                $keyboard['inline_keyboard'][] = [
+                    ['text' => $this->translate('link_telegram'), 'callback_data' => 'author:link_telegram:' . $authorId],
+                ];
+            }
+        }
+
+        if ($this->isGranted(AuthorVoter::DELETE, $author)) {
+            $keyboard['inline_keyboard'][] = [
+                ['text' => $this->translate('delete_this_author'), 'callback_data' => 'author:to_delete:' . $authorId],
+            ];
+        }
+
+        $keyboard['inline_keyboard'][] = [
+            ['text' => $this->translate('go_back'), 'callback_data' => $currentPage ? $currentPage : 'admin:panel'],
+        ];
+
+        $text = str_replace(
+            ['{author}', '{biography}', '{link}'],
+            [
+                htmlspecialchars($author->getName()),
+                ($author->getBiography() ? htmlspecialchars($author->getBiography()) : $this->translate('bio_not_set')),
+                ($author->getChannelLink() ? htmlspecialchars($author->getChannelLink()) : $this->translate('link_not_set'))
+            ],
+            $this->translate('author_info_message')
+        );
+
+        $this->renderPanel($chatId, $userId, $visualsLinks[11], $text, $keyboard);
     }
 }
